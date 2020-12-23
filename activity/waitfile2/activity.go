70974331a2771561cd81c536816a9ff84bcd872c
xchuanzhang <xchuanzhang@tencent.com>
2020-12-23 19:01:05 +0800

    waitfile

---
+++ activity/waitfile2/activity.go
@@ -0,0 +1,39 @@
+package waitfile
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// activityLog is the default logger for the Log Activity
+var activityLog = logger.GetLogger("activity-flogo-fileexists")
+
+const (
+	url     = "url"
+)
+
+// SleepActivity is an Activity that can stop flow execution for given time duration.
+// inputs : {interval, intervalType}
+// outputs: none
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new AppActivity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata returns the activity's metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	//mv := context.GetInput(ivMessage)
+	url := context.GetInput(url).(string)
+	activityLog.Infof(url)
+	context.SetOutput("status",true)
+	return true, nil
+}
---
+++ activity/waitfile2/go.mod
@@ -0,0 +1,7 @@
+module github.com/goldboy123/flogo_tests/activity/waitfile2
+
+
+require (
+	github.com/TIBCOSoftware/flogo-contrib v0.5.8
+	github.com/TIBCOSoftware/flogo-lib v0.5.8
+)
