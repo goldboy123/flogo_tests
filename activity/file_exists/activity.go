0522efcf327ede3f5755ab603a1a1b06abf67055
xchuanzhang <xchuanzhang@tencent.com>
2020-12-23 17:10:15 +0800

    file exists

--- activity/file_exists/activity.go
+++ activity/file_exists/activity.go
@@ -2,80 +2,28 @@ package file_exists
 
 import (
 	"github.com/project-flogo/core/activity"
-	"github.com/project-flogo/core/data/coerce"
 )
 
-func init() {
-	_ = activity.Register(&Activity{})
-}
-
-type Output struct {
-	Status bool `md:"file exists"` // the file is exists
-}
-
-func (o Output) ToMap() map[string]interface{} {
-	return map[string]interface{}{
-		"status" : o.Status,
-	}
-}
-
-func (o Output) FromMap(values map[string]interface{}) error {
-	var err error
-	o.Status,err = coerce.ToBool(values["status"])
-	if err != nil{
-		return err
-	}
-	return nil
-}
-
-type Input struct {
-	Url string `md:"file url"` // the url to request
-}
-
-
-func (i Input) ToMap() map[string]interface{} {
-	return map[string]interface{}{
-		"url" : i.Url,
-	}
-}
-
-
-
-func (i Input) FromMap(values map[string]interface{}) error {
-	var err error
-	i.Url, _ = coerce.ToString(values["url"])
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-
-
-var activityMd = activity.ToMetadata(&Input{})
-
 // Activity is an Activity that is used to log a message to the console
-// inputs : none
-// outputs: none
-type Activity struct {
+// inputs : url
+// outputs: status
+type MyActivity struct {
 	metadata *activity.Metadata
 }
 
 func NewActivity(metadata *activity.Metadata) activity.Activity{
-	return &Activity{metadata:metadata}
+	return &MyActivity{metadata:metadata}
 }
 
 // Metadata returns the activity's metadata
-func (a *Activity) Metadata() *activity.Metadata {
+func (a *MyActivity) Metadata() *activity.Metadata {
 	return a.metadata
 }
 
 // Eval implements api.Activity.Eval
-func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
-	input := &Input{}
-	ctx.GetInputObject(input)
+func (a *MyActivity) Eval(ctx activity.Context) (done bool, err error) {
 
-	url := input.Url
+	url := ctx.GetInput("url").(string)
 	ctx.Logger().Debugf(url)
 	//output := &Output{Status:true}
 	err = ctx.SetOutput("status",true)
